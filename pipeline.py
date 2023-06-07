8e84692134de81ef1eac1b7cad475924f6ba3552
arkiver <arkiver@protonmail.com>
2023-06-07 18:30:31 +0200

    Version 20230607.01. Use GNU Wget 1.21.3-at.20230605.01 and arguments around DNS.

--- pipeline.py
+++ pipeline.py
@@ -37,9 +37,7 @@ if StrictVersion(seesaw.__version__) < StrictVersion('0.8.5'):
 WGET_AT = find_executable(
     'Wget+AT',
     [
-        'GNU Wget 1.20.3-at.20211001.01',
-        'GNU Wget 1.21.3-at.20220503.02',
-        'GNU Wget 1.21.3-at.20220608.02'
+        'GNU Wget 1.21.3-at.20230605.01'
     ],
     [
         './wget-at',
@@ -56,7 +54,7 @@ if not WGET_AT:
 #
 # Update this each time you make a non-cosmetic change.
 # It will be added to the WARC files and reported to the tracker.
-VERSION = '20221223.01'
+VERSION = '20230607.01'
 USER_AGENT = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:90.0) Gecko/20100101 Firefox/90.0'
 TRACKER_ID = 'youtube'
 TRACKER_HOST = 'legacy-api.arpa.li'
@@ -199,6 +197,10 @@ class WgetArgs(object):
             WGET_AT,
             '-U', USER_AGENT,
             '-nv',
+            '--host-lookups', 'dns',
+            '--hosts-file', '/dev/null',
+            '--resolvconf-file', '/dev/null',
+            '--dns-servers', '9.9.9.10,149.112.112.10,2620:fe::10,2620:fe::fe:10',
             '--content-on-error',
             '--load-cookies', 'cookies.txt',
             '--lua-script', 'youtube.lua',

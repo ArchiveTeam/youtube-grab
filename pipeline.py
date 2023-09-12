8547a575293cb89db4c97593fbbad06d184b4dea
arkiver <arkiver@protonmail.com>
2023-09-12 02:44:17 +0200

    Version 20230912.01. Prevent consent redirect.

--- pipeline.py
+++ pipeline.py
@@ -54,7 +54,7 @@ if not WGET_AT:
 #
 # Update this each time you make a non-cosmetic change.
 # It will be added to the WARC files and reported to the tracker.
-VERSION = '20230727.01'
+VERSION = '20230912.01'
 USER_AGENT = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:90.0) Gecko/20100101 Firefox/90.0'
 TRACKER_ID = 'youtube'
 TRACKER_HOST = 'legacy-api.arpa.li'

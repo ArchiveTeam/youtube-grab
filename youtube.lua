fd647c16a0031befc3e6cd2e0427dd489ede8fcb
arkiver <arkiver@protonmail.com>
2025-08-20 11:34:57 +0200

    Version 20250820.03. Improve audio checking print string.

--- youtube.lua
+++ youtube.lua
@@ -720,7 +720,7 @@ wget.callbacks.get_urls = function(file, url, is_css, iri)
         if format["drmFamilies"] then
           drm = true
         end
-        print("Checking audio " .. name .. " with bitrate " .. bitrate .. ", quality " .. quality .. ", DRC " .. tostring(drc) .. ", codec " .. codec .. ", DRM " .. tostring(drm))
+        print("Checking audio" .. name_string .. " with bitrate " .. bitrate .. ", quality " .. quality .. ", DRC " .. tostring(drc) .. ", codec " .. codec .. ", DRM " .. tostring(drm))
         if not drm
           and (
             not current_audio_url[name]

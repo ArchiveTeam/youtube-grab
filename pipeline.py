15159c07f4d79204deebac9e50545c4cd3e45645
arkiver <arkiver@protonmail.com>
2026-01-17 06:12:14 +0100

    Version 20260117.03.

--- pipeline.py
+++ pipeline.py
@@ -70,7 +70,7 @@ if not WGET_AT:
 #
 # Update this each time you make a non-cosmetic change.
 # It will be added to the WARC files and reported to the tracker.
-VERSION = '20260117.01'
+VERSION = '20260117.03'
 USER_AGENT = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:136.0) Gecko/20100101 Firefox/136.0'
 TRACKER_ID = 'youtube'
 TRACKER_HOST = 'legacy-api.arpa.li'
